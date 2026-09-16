e24d3c415bed635dcd02d648baf3ae85d55cbb29
Ilya_Komashilo <ilya.komash@gmail.com>
2026-09-16 20:34:14 +0300

    simplify routes

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,6 +4,29 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.database import get_db
+from src.repositories.notifications import NotificationsRepository
+from src.repositories.templates import TemplatesRepository
+from src.services.notification_service import NotificationService
+from src.services.template_service import TemplateService
 
 
-SessionDep = Annotated[AsyncSession, Depends(get_db),]
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
+
+
+async def get_template_service(session: SessionDep) -> TemplateService:
+    repository = TemplatesRepository(session)
+    return TemplateService(repository)
+
+
+TemplateServiceDep = Annotated[TemplateService, Depends(get_template_service)]
+
+
+async def get_notification_service(
+    session: SessionDep,
+    template_service: TemplateServiceDep,
+) -> NotificationService:
+    repository = NotificationsRepository(session)
+    return NotificationService(repository, template_service)
+
+
+NotificationServiceDep = Annotated[NotificationService, Depends(get_notification_service)]
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from src.api.exception_handlers import register_exception_handlers
 from src.api.health import router as health_router
 from src.api.templates import router as template_router
 from src.api.notifications import router as notification_router
@@ -7,9 +8,10 @@ app = FastAPI(
     version="0.1.0"
 )
 
+register_exception_handlers(app)
+
 app.include_router(health_router)
 app.include_router(template_router)
 app.include_router(notification_router)
 
 # uvicorn src.main:app --reload --port 9000
-
